b037dc4270cb296293d0c9da7219c99ba08545b3
Randi Schreiner <mimirandi21@gmail.com>
2020-11-30 14:01:45 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post = Post.find(params[:id])
+    if @post.update(post_params) == true
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
